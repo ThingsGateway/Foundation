09937c8065abd62177ce6f6082b9a0c182a3476e
Diego <2248356998@qq.com>
2026-02-05 17:36:30 +0800

    增加demo

---
+++ sample/ThingsGateway.Foundation.Sample/deviceDemo/DltTest.cs
@@ -0,0 +1,40 @@
+﻿using ThingsGateway.Foundation;
+using ThingsGateway.Foundation.Common;
+using ThingsGateway.Foundation.Dlt645;
+using TouchSocket.Core;
+using TouchSocket.Sockets;
+
+#pragma warning disable CA2000 // 丢失范围之前释放对象
+namespace ThingsGateway.Foundation.Sample
+{
+    internal class DltTest
+    {
+
+        /// <summary>
+        /// 新建协议对象
+        /// </summary>
+        /// <returns></returns>
+        public Dlt645_2007Master GetDevice()
+        {
+            var clientConfig = new TouchSocket.Core.TouchSocketConfig();
+            var client = new Dlt645_2007Master();
+            var clientChannel = client.CreateChannel(clientConfig, new ChannelOptions() { ChannelType = ChannelTypeEnum.SerialPort, PortName = "COM1" });
+            client.InitChannel(new(clientChannel));
+
+            client.Station = "311111111114";//表号
+
+            return client;
+        }
+
+
+        public async Task Test()
+        {
+            var dlt2007 = GetDevice();
+
+            var data = await dlt2007.ReadDoubleAsync("02010100").ConfigureAwait(false); //读取A相电压
+            Console.WriteLine(data.IsSuccess ? $"A相电压：{data}" : data.ToString());
+        }
+
+
+    }
+}
---
+++ sample/ThingsGateway.Foundation.Sample/deviceDemo/ModbusTest.cs
@@ -0,0 +1,53 @@
+﻿using ThingsGateway.Foundation;
+using ThingsGateway.Foundation.Common;
+using ThingsGateway.Foundation.Dlt645;
+using ThingsGateway.Foundation.Modbus;
+using TouchSocket.Core;
+using TouchSocket.Sockets;
+#pragma warning disable CA2000 // 丢失范围之前释放对象
+
+namespace ThingsGateway.Foundation.Sample
+{
+    internal class ModbusTest
+    {
+
+        /// <summary>
+        /// 新建协议对象
+        /// </summary>
+        /// <returns></returns>
+        public ModbusMaster GetDevice()
+        {
+            var clientConfig = new TouchSocket.Core.TouchSocketConfig();
+            var client = new ModbusMaster();
+            var clientChannel = client.CreateChannel(clientConfig, new ChannelOptions() { ChannelType = ChannelTypeEnum.SerialPort, PortName = "COM1" });
+            client.InitChannel(new(clientChannel));
+
+            client.Station = 1;
+
+            return client;
+        }
+
+
+        public async Task Test()
+        {
+            var modbus = GetDevice();
+            {
+                var data = await modbus.ReadAsync("0;S=1;F=3", 1).ConfigureAwait(false); //读取保持寄存器地址0，长度1的值
+                if (data.IsSuccess)
+                {
+                    var uint16 = modbus.BitConverter.ToUInt16(data.Content.Span, 0);//自行解析
+                }
+
+                var int32Result = await modbus.ReadInt32Async("0;S=1;F=3", 1).ConfigureAwait(false); //读取保持寄存器地址0，长度2个modbus寄存器的值
+
+
+
+            }
+            {
+                var data = await modbus.ReadAsync("0;S=1;F=3", 1).ConfigureAwait(false); //读取保持寄存器地址0，长度1的值
+            }
+        }
+
+
+    }
+}
